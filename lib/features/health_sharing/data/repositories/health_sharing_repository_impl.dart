2b541e7645d8631b8307f51ab647abe2779547d5
iberi22 <10615454+iberi22@users.noreply.github.com>
2026-07-03 05:46:31 +0000

    fix(health_sharing): add repository abstraction to domain layer
    - Create SharingRepository abstract class in domain layer
    - Rename HealthSharingRepository to HealthSharingRepositoryImpl and implement SharingRepository
    - Update dependency injection and relevant tests

--- lib/features/health_sharing/data/repositories/health_sharing_repository_impl.dart
+++ lib/features/health_sharing/data/repositories/health_sharing_repository_impl.dart
@@ -1,11 +1,19 @@
+import 'package:injectable/injectable.dart';
 import '../../domain/entities/shared_health_package.dart';
+import '../../domain/repositories/sharing_repository.dart';
 import '../datasources/health_sharing_local_datasource.dart';
 
-class HealthSharingRepository {
+@LazySingleton(as: SharingRepository)
+class HealthSharingRepositoryImpl implements SharingRepository {
   final HealthSharingLocalDataSource _localDataSource;
-  HealthSharingRepository(this._localDataSource);
+  HealthSharingRepositoryImpl(this._localDataSource);
 
+  @override
   Future<List<SharedHealthPackage>> getSentPackages() => _localDataSource.getSentPackages();
+
+  @override
   Future<List<SharedHealthPackage>> getReceivedPackages() => _localDataSource.getReceivedPackages();
+
+  @override
   Future<void> sendPackage(SharedHealthPackage pkg) async => _localDataSource.savePackage(pkg);
 }
