b9dc9297a4bad8e5af9b460919115537187cf083
iberi22 <10615454+iberi22@users.noreply.github.com>
2026-07-04 09:28:41 +0000

    feat(voice_chat): add golden tests for pages and widgets
    - Organize golden tests in `test/features/voice_chat/presentation/golden/`
    - Add dedicated golden tests for VoiceChatPage, ChatPage, and PrivacyPolicyPage
    - Add golden tests for MessageBubble and VoiceInputButton widgets
    - Reference existing golden images in `golden/reference/`
    - Remove redundant `test/features/voice_chat/presentation/voice_chat_golden_test.dart`

---
+++ test/features/voice_chat/presentation/golden/chat_page_golden_test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:get_it/get_it.dart';
+import 'package:orionhealth_health/features/voice_chat/presentation/pages/chat_page.dart';
+import 'package:orionhealth_health/features/voice_chat/application/voice_chat_cubit.dart';
+import 'package:orionhealth_health/features/voice_chat/application/voice_chat_state.dart';
+import 'package:orionhealth_health/core/services/aicore_service.dart';
+import '../../../../core/golden_test_utils.dart';
+
+class MockVoiceChatCubit extends Mock implements VoiceChatCubit {}
+class MockAIService extends Mock implements AIService {}
+
+void main() {
+  late MockVoiceChatCubit mockCubit;
+  late MockAIService mockAIService;
+
+  setUpAll(() {
+    registerFallbackValue(const VoiceChatState());
+  });
+
+  setUp(() async {
+    mockCubit = MockVoiceChatCubit();
+    mockAIService = MockAIService();
+    final getIt = GetIt.I;
+    if (getIt.isRegistered<VoiceChatCubit>()) {
+      await getIt.unregister<VoiceChatCubit>();
+    }
+    if (getIt.isRegistered<AIService>()) {
+      await getIt.unregister<AIService>();
+    }
+    getIt.registerSingleton<VoiceChatCubit>(mockCubit);
+    getIt.registerSingleton<AIService>(mockAIService);
+
+    when(() => mockCubit.state).thenReturn(const VoiceChatState());
+    when(() => mockCubit.stream).thenAnswer((_) => const Stream.empty());
+    when(() => mockCubit.init()).thenAnswer((_) async {});
+    when(() => mockCubit.close()).thenAnswer((_) async {});
+    when(() => mockAIService.currentState).thenReturn(AIServiceState.ready);
+  });
+
+  tearDown(() async {
+    await GetIt.I.reset();
+  });
+
+  group('ChatPage Golden Tests', () {
+    testWidgets('ChatPage - Default State', (tester) async {
+      setupGoldenTest(tester);
+      await tester.pumpWidget(wrapWithMaterial(const ChatPage()));
+      await tester.pumpAndSettle();
+
+      await expectLater(
+        find.byType(ChatPage),
+        matchesGoldenFile("../../../../../golden/reference/voice_chat_page_alt.png"),
+      );
+      resetGoldenTest(tester);
+    });
+  });
+}
